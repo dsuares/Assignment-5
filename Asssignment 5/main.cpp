3c9e5318c5c4332c4f0d2e83f95d4ec991723b7e
Dominic Suares <dominicsuares@dominics-macbook-pro.local>
2015-11-16 13:39:31 -0800

    Initial Commit

---
+++ Asssignment 5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Asssignment 5
+//
+//  Created by Dominic Suares on 11/16/15.
+//  Copyright © 2015 Dominic Suares. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
