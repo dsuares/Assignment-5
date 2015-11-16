c05657798a93cca7c32d352218febbbeb20fda7a
Dominic Suares <dominicsuares@dominics-macbook-pro.local>
2015-11-16 14:44:32 -0800

    Version 2

--- Asssignment 5/main.cpp
+++ Asssignment 5/main.cpp
@@ -7,9 +7,33 @@
 //
 
 #include <iostream>
+#include <cstdlib>
+#include <time.h>
+
+using namespace::std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
     return 0;
 }
+
+class Scheduler{
+private:
+    Patient patient_list[1024];
+    
+}
+
+class Patient : Appointment {
+public:
+    string name;
+    int medical_record_number = 0;
+    int patient_zip_code = 0;
+    double balance = 0.0;
+    
+    
+}
+
+class Appointment {
+    
+};
